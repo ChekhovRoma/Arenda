5a856c34177b15277fcb101056b6f55b0a4d60bb
Sevak <sevak754@gmail.com>
2020-05-16 18:21:31 +0300

    Добавил вывод объявлений

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,30 @@ header( 'Access-Control-Allow-Headers: Authorization, Content-Type' );
 Route::get('form', 'FormController@create')->name('form.create');
 Route::post('form', 'FormController@store')->name('form.store');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'AdController@getAllAds');
 
-Route::post('/test', 'Place@test');
-Route::post('/getSchemaById', 'Place@getSchemaById');
+Route::post('/test', 'PlaceController@test');
+Route::post('/getSchemaById', 'PlaceController@getSchemaById');
 
 Route::get('/editor', 'EditorController@index');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')->group(function (){
+    Route::get('/myAds', 'AdController@getUserAds')->name('my-ads');
+    Route::get('/createAd',function () {
+        echo 'Создание объявления';
+    })->name('create-ad');
+});
+Route::post('/filter', function (){
+    echo 'Romchik detected';
+})->name('filter');
+
+
+Route::get('/ad/{id}', function ($id) {
+    echo $id;
+});
+
+
